6f886f183eed2c727d78e2cf83aad3e473dab409
hannazo <106128732+hannazo@users.noreply.github.com>
2023-03-13 09:59:39 -0400

    added uuid npm

--- routes/notes.js
+++ routes/notes.js
@@ -1,29 +1,30 @@
 const app = require('express').Router();
 const { readFromFile, readAndAppend } = require('../helpers/fsNotes');
-const path = require('path');
+const { v4: uuidv4 } = require('uuid');
 
 // GET Route for retrieving all the notes
 app.get('/', (req, res) => {
-    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
-  });
-  
-  // POST Route for a new note
-  app.post('/', (req, res) => {
-    console.log(req.body);
-  
-    const { title, text } = req.body;
-  
-    if (req.body) {
-      const newNote = {
-        title,
-        text
-      };
-  
-      readAndAppend(newNote, './db/db.json');
-      res.json(newNote);
-    } else {
-      res.error('Error in saving note');
-    }
-  });
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+// POST Route for a new note
+app.post('/', (req, res) => {
+  console.log(req.body);
+
+  const { title, text } = req.body;
+
+  if (req.body) {
+    const newNote = {
+      title,
+      text,
+      id: uuidv4()
+    };
+
+    readAndAppend(newNote, './db/db.json');
+    res.json(newNote);
+  } else {
+    res.error('Error in saving note');
+  }
+});
 
 module.exports = app;
